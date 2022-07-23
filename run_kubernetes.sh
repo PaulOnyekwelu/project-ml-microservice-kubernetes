a172196553d18c1c2f8934b9adf37dbaeaca3eb7
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2022-07-23 17:16:58 +0100

    Task 6: Deploy with Kubernetes and Save Output Logs

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,17 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=silanka/ml_mcs_kub_image
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl create deploy kub-ml-app --image=silanka/ml_mcs_kub_image:latest
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward pod/kub-ml-app-dc564fbb4-wk8ns --address 0.0.0.0 8000:80
 
