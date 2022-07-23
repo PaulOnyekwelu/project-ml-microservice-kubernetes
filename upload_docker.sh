81cc5aa970e56776aeb31f7db1cf3a907fbaa8fb
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2022-07-23 14:26:03 +0100

    Task 4: Upload the Docker Image

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=silanka/ml_mcs_kub_image
 
 # Step 2:  
 # Authenticate & tag
+docker login
+docker tag ml_mcs_kub_image $dockerpath
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
